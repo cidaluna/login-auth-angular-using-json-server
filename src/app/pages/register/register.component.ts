4f30092618b963dfb0da5baab3c5596e191aabd4
Cida Luna <cidalunna@hotmail.com>
2024-10-17 13:16:47 -0300

    Add validators on name, lastname, email and password input

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -25,15 +25,27 @@ export class RegisterComponent implements OnInit{
     'Doutorado'
   ];
 
+  msg_error_password: string = `Senha inválida. A senha deve ter entre 4 e 6 dígitos,
+  incluindo números e um desses caracteres especiais !, #, %, @.`;
+
   constructor(private _fb: FormBuilder,
     private _registerUserService: RegisterUserService,
     private _router: Router
     ){
     this.registerForm = this._fb.group({
-      name: ['', Validators.required],
-      lastname: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
+      name: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$"),
+        ]
+      ],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(6),
+        Validators.pattern("^(?=.*[0-9])(?=.*[!#%@]).{4,6}$"),
+      ]],
       education: ['', Validators.required],
       gender: ['', Validators.required],
       startDate: ['', Validators.required]
